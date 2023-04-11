d463fe2d50a72dd120a9b47776c9c4f2e87087d0
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2023-04-11 05:23:43 -0400

    initial commit

---
+++ messages.py
@@ -0,0 +1,34 @@
+subject = "Hello! Let's Boost your profits with affordable Software Development"
+messages = {
+    "subject": "Hello! Let's Boost your profits with affordable Software Development",
+    "serviceProvider":
+        """
+        Nothing.
+
+        """,
+        "Plumbing Services":
+        """
+        <p style="font-size: 16px; line-height: 1.5;">Dear <client_name>,</p>
+
+        <p style="font-size: 16px; line-height: 1.5;">I hope this email finds you well. As a software developer and SEO consultant, I understand the importance of having a strong online presence in today's digital age. That's why I am reaching out to offer my services to help you achieve your online goals.</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">By creating a responsive and accessible website, you can have full control over your content and target your own audience. This will help you build trust with your customers and increase brand recognition. In addition, my SEO management services will increase the reach of potential clients and boost the number of visits, customers, sales, and conversions to your website. You will also see an increase in subscribers, followers, likes, and shares.</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">These services are tailored to meet the unique needs of your plumbing business in Toronto and can help you achieve your goals, such as increasing your online visibility and boosting your profits.</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">With my help, you can focus on your business and let me handle your online presence. Some clients have experienced impressive revenue growth of up to 40%</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">Lastly, I would like to emphasize that working with me is completely safe and reliable. To ensure my clients' satisfaction, I only get paid after completing milestones in the project. This means that you don't have to worry about any upfront costs, and you can rest assured that I will deliver quality work before receiving any payment. My goal is to provide you with a stress-free and transparent experience while achieving your business objectives.</p>
+
+        <p style="font-size: 16px; line-height: 1.5;">Please let me know if you are interested, and we can set up a call to discuss your goals.</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">Thank you for considering my services, and I look forward to hearing from you soon.</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">Best regards,</p>
+        
+        <p style="font-size: 16px; line-height: 1.5;">Yuri Almeida</p> 
+
+        <p style="font-size: 16px; line-height: 1.5;"><a text-decoration: underline; href='https://yalmeida-nu.vercel.app/' target="_blank">Visit my website!</a></p>        
+        """
+
+}
---
+++ proposals.py
@@ -0,0 +1,142 @@
+import csv
+from datetime import datetime
+import time
+import json
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from messages import messages, subject
+import os
+
+
+
+bot_mail = os.environ.get('BOT_EMAIL')
+bot_path = os.path.abspath(__file__).replace('\\', '/').replace('local_bot.py', '')
+
+class LocalBot:
+    def __init__(self, local_path=bot_path):
+        self.local_path = local_path    
+    def send_mail(self, to_emails:str, html_content, from_email:str=bot_mail, subject:str="An email scheduled by LocalBot!", time:int=126):
+        """
+        # Send an email with SendGrid API.
+
+        ## Parameters:
+        - to_emails (str): A comma-separated string containing one or more email addresses of the recipient(s).
+        - html_content (str): The HTML content of the email message.
+        - from_email (str): The email address of the sender. Defaults to `bot_email`.
+        - subject (str): The subject of the email message. Defaults to "An email scheduled by LocalBot!".
+        - time (int): The number of seconds to wait before sending the email message. Defaults to 126 seconds (i.e., 2 minutes and 6 seconds).
+
+        ## Returns:
+        - None: This function does not return a value.
+
+        ## Raises:
+        - Exception: If an error occurs while sending the email, this function raises an Exception with a descriptive error message.
+
+        ## Example usage:
+        >>> send_mail('user@example.com', '<p>Hello, world!</p>')
+        """        
+        message = Mail(
+            from_email=from_email,
+            to_emails=to_emails,
+            subject=subject,
+            html_content=html_content)
+            
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            return f'Response: {response.status_code}'
+        except Exception as e:
+            print(e.to_dict)
+        # except Exception as e:
+        #     return e
+
+bot = LocalBot()
+
+date = datetime.now().strftime("%d/%m/%Y")
+time = datetime.now().strftime("%H:%M:%S")
+def save_to_csv(client_name, client_email, client_category):
+    """
+    Saves the client information to a csv file.
+    """
+    try:
+        with open('sentProposals.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([client_name, client_email, client_category, date, time])
+    except Exception as e:
+        print(e)
+
+
+
+
+def load_emails_from_csv(file_name):
+    """Load email addresses from a CSV file."""
+    with open(file_name, 'r', newline='') as f:
+        potential_clients = csv.DictReader(f)
+        reader = []
+        for row in potential_clients:
+            row = dict(row)
+            reader.append(row)
+    potential_clients = reader
+
+    return potential_clients
+
+
+potential_clients = load_emails_from_csv('proposals.csv')
+sent_emails = load_emails_from_csv('sentProposals.csv')
+
+
+def load_emails_from_csv(file_name):
+    """Load email addresses from a CSV file."""
+    with open(file_name, 'r', newline='') as f:
+        potential_clients = csv.DictReader(f)
+        reader = []
+        for row in potential_clients:
+            row = dict(row)
+            reader.append(row)
+    potential_clients = reader
+
+    return potential_clients
+
+
+
+def find_duplicates(potential_clients):
+    """Remove duplicate clients from the potential_clients list."""
+    emails = {client["Email"] for client in sent_emails}
+    duplicate_clients = [client for client in potential_clients if client["Email"] in emails]
+    potential_clients[:] = [client for client in potential_clients if client not in duplicate_clients]
+
+    if duplicate_clients:
+        print(f'\nDUPLICATE - Removed from potential clients list:\n{[client["Email"] for client in duplicate_clients]}\n')
+
+    return potential_clients
+
+def send_proposals_to_clients(clients, messages, category):
+    """Send proposals to clients based on the specified category."""
+    if len(clients) > 0:
+        print(f'\nSending proposals to {len(clients)} {category} clients...')
+        for client in clients:
+            message = messages[category].replace("<client_name>", client.get("company_name", client["Name"]))
+            response = bot.send_mail(client["Email"], subject=subject, html_content=message)
+            if str(response) != "HTTP Error 400: Bad Request":
+                print(f'\nResponse {response}')
+                print(f'\nResponse type {type(response)}')
+                print(f'\nProposal sent to {client.get("company_name", client["Name"])}')
+                save_to_csv(client.get("company_name", client["Name"]), client["Email"], client["Category"])
+    else:
+        print(f'\nNo {category} clients to send proposals to.')
+
+def send_proposals():
+    """Send proposals to potential clients."""
+    plumbers = [client for client in potential_clients if client["Category"] == "Plumbing Services"]
+    service_providers = [client for client in potential_clients if client["Category"] == "serviceProvider"]
+
+    find_duplicates(plumbers)
+    find_duplicates(service_providers)
+
+    send_proposals_to_clients(plumbers, messages, "Plumbing Services")
+    send_proposals_to_clients(service_providers, messages, "serviceProvider")
+
+    print("\nDone!")
+
+if __name__ == "__main__":
+    send_proposals()
